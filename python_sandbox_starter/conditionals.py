c5f2a97cad580d67f3b7eec054da45560bd2ee04
Brian Pino <brianpino@bpmbp.local>
2019-02-15 14:52:25 -0800

    Setup project scaffolding

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,8 +1,29 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
+x = 10
+y = 10
 
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
+
+# Simple If
+
+if x == y:
+    print(f'{x} is equal to {y}')
+
+if x > y:
+    print(f'{x} is greater than {y}')
+else:
+    print(f'{x} is less than or equal to {y}')
+
+
+
+if x > y:
+    print(f'{x} is greater than {y}')
+elif x == y:
+    print(f'{x} is equal to {y}')
+else:
+    print(f'{x} is less than {y}')
 
 
 
