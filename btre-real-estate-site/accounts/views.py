1987f2257693b024a108d5a4b6941358fb676806
Brian Pino <brianpino@gmail.com>
2019-02-23 14:56:35 -0800

    Basic login/register setup

---
+++ btre-real-estate-site/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        # Register user
+        print()
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        # Login user
+        print()
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
