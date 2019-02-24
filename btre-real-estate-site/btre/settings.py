44bda84befa480c04bb21fd37549f345ae30a942
Brian Pino <brianpino@gmail.com>
2019-02-24 12:33:07 -0800

    Added email for inquiries, and showing inquiries on dashboard

--- btre-real-estate-site/btre/settings.py
+++ btre-real-estate-site/btre/settings.py
@@ -141,5 +141,11 @@ MEDIA_URL = '/media/'
 # Messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-
 }
+
+# Email config (NOT COMPLETE)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
--- btre-real-estate-site/contacts/views.py
+++ btre-real-estate-site/contacts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from django.contrib import messages
 from .models import Contact
 # Create your views here.
@@ -15,10 +16,29 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Check for existing submission
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    request, 'You have already submitted an inquiry for this listing')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name,
                           email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        # Send email
+        send_mail(
+            'Property Inquiry',
+            f'You have a new inquiry for {listing}. Sign into the admin dashboard for more info',
+            'testEmail@gmail.com',
+            [realtor_email],
+            fail_silently=False
+        )
         messages.success(
             request, 'Your request has been submitted, a realtor will get to you in 2-3 business days')
         return redirect('/listings/'+listing_id)
